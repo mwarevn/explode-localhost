f63dc59db41e00cd8e145ef64f33c08e24e0a8a4
MWare <124526287+mwarevn@users.noreply.github.com>
2023-06-21 15:29:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,60 +2,71 @@ import platform
 import urllib.request
 import os
 import psutil
+import time
+import json
+import signal
+import sys
 
 def kill_process_by_name(process_name):
+    if os.path.exists('cloudflared.json'):
+        os.remove('cloudflared.json')
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name:
-            pid = proc.info['pid']
-            process = psutil.Process(pid)
-            process.terminate()
+            proc.terminate()
             return True
     return False
 
 kill_process_by_name("cloudflared")
+
+def exit_handler(signal, frame):
+    kill_process_by_name('cloudflared')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_handler)
+
 architecture = platform.machine()
 
 def tunnel(ip, port):
     print("Starting server cloudflared...")
-    os.system("./cloudflared tunnel -url " + ip+":"+port)
+    os.system(f"./cloudflared tunnel -url {ip}:{port} --logfile cloudflared.json > /dev/null 2>&1 &")
+    time.sleep(8)
+
+    filename = "cloudflared.json"
+
+    with open(filename, "r") as file:
+        data = file.readlines()
+
+    url = next((json.loads(line)["message"].strip().split()[1] for line in data if "message" in json.loads(line) and ".trycloudflare.com" in json.loads(line)["message"]), None)
+
+    if url:
+        print("Your URL is: " + url)
+        while os.path.exists('cloudflared.json'):
+            time.sleep(2)
+    else:
+        print("URL not found in the file.")
 
 def input_localhost():
-    ip="127.0.0.1"
-    port="3000"
-
-    input_ip = input("Enter your network ip (default is 127.0.0.1): ")
-    input_port = input("Enter your port (default is 3000): ")
-
-    if input_port != "":
-        port=input_port
-
-    if input_ip != "":
-        ip=input_ip
-
+    ip = input("Enter your network IP (default is 127.0.0.1): ") or "127.0.0.1"
+    port = input("Enter your port (default is 3000): ") or "3000"
     tunnel(ip, port)
-
 
 def download_cloudflared():
     print("Downloading cloudflared...")
-    url = ""
-    filename = "cloudflared"
-
-    if architecture == 'arm' or architecture == 'Android':
-        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm"
-    elif architecture == 'aarch64':
-        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64"
-    elif architecture == 'x86_64':
-        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64"
-    else:
-        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386"
-
-    # Download the file
+    architecture_map = {
+        'arm': 'cloudflared-linux-arm',
+        'aarch64': 'cloudflared-linux-arm64',
+        'x86_64': 'cloudflared-linux-amd64',
+        'Android': 'cloudflared-linux-arm'  # Assuming Android uses ARM architecture
+    }
+    filename = architecture_map.get(platform.machine(), 'cloudflared-linux-386')
+    url = f"https://github.com/cloudflare/cloudflared/releases/latest/download/{filename}"
     urllib.request.urlretrieve(url, filename)
+    os.rename(filename, 'cloudflared')  # Rename the file
     print("Download completed!")
     input_localhost()
 
 if os.path.exists("cloudflared"):
-    print("File cloudflared exists.")
+    print("File cloudflared installed!")
     input_localhost()
 else:
     download_cloudflared()
